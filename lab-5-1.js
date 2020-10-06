0b12d6680d3249e61cd3a63bb1794a95ddfe1747
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-10-06 16:31:07 -0400

    final changes to part 1

--- lab-5-1.js
+++ lab-5-1.js
@@ -10,29 +10,25 @@ function calcAverage(num1, num2, num3, num4, num5) {
     if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4) ||isNaN(num5)) {
         throw("ONLY NUMBERS ACCEPTED");
     } else {
-        var sum = num1 + num2 + num3 + num4 + num5;
-        var average = sum / 5;
-        //Print to console the average of each number to one decimal place.
-        console.log(average.toFixed(1));
-        //If average is greater than 70, let the user know their status
-        if (average > 70.0) {
-            alert("You're a winner.");
-        }
+        //Create a variable the sum of all arguments
+        let sum = num1 + num2 + num3 + num4 + num5;
+        let average = sum / 5;
         //return the average to one decimal place
         return average.toFixed(1);
     }
 }
-
+//Print to console the average of each number to one decimal place.
+console.log(average.toFixed(1));
 //Humber Web Development object, courses as properties with `
-var webCourse1 = 96;
-var webCourse2 = 90;
-var webCourse3 = 90;
+var webCourse1 = 9;
+var webCourse2 = 9;
+var webCourse3 = 9;
 var webCourse4 = 82;
 var webCourse5 = 80;
 
-
-
-
-
-
-//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+//If average is greater than 70, let the user know their status
+if (calcAverage(webCourse1,webCourse2, webCourse3, webCourse4, webCourse5) > 70.0) {
+    alert("You're a winner.");
+} else {
+    alert("Review Required");
+}
