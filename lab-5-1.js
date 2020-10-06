76855df5b4576f893dc733606570384064ef90ef
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-10-06 15:27:50 -0400

    added webDev object

--- lab-5-1.js
+++ lab-5-1.js
@@ -7,8 +7,16 @@ function calcAverage(num1, num2, num3, num4, num5) {
     var sum = num1 + num2 + num3 + num4 + num5;
     var average = sum / 5;
     console.log(average.toFixed(1));
+    return average;
 }
 
+var webDev = {
+    projectManagement: 96,
+    webProgramming: 90,
+    webAppDevelopment: 90,
+    digitalDesign: 82,
+    databaseDesign: 80
+};
 
 
 //################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
