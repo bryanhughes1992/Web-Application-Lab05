33adc48e3c1d56519b0bba4f54f2d8b39d9de282
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-10-06 16:05:59 -0400

    Part one final touches

--- lab-5-1.js
+++ lab-5-1.js
@@ -3,20 +3,36 @@
 //alert("Connected");//COMMENT OUT AS SOON AS YOU KNOW YOU ARE CONNECTED!!!!
 
 //################## CREATE YOUR AVERAGE FUNCTION
+
+//Function takes five numbers and calculates the average.
 function calcAverage(num1, num2, num3, num4, num5) {
-    var sum = num1 + num2 + num3 + num4 + num5;
-    var average = sum / 5;
-    console.log(average.toFixed(1));
-    return average;
+    //Validate arguments as numbers
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4) ||isNaN(num5)) {
+        throw("ONLY NUMBERS ACCEPTED");
+    } else {
+        var sum = num1 + num2 + num3 + num4 + num5;
+        var average = sum / 5;
+        //Print to console the average of each number to one decimal place.
+        console.log(average.toFixed(1));
+        //If average is greater than 70, let the user know their status
+        if (average > 70.0) {
+            alert("You're a winner.");
+        }
+        //return the average to one decimal place
+        return average.toFixed(1);
+    }
 }
 
-var webDev = {
-    projectManagement: 96,
-    webProgramming: 90,
-    webAppDevelopment: 90,
-    digitalDesign: 82,
-    databaseDesign: 80
-};
+//Humber Web Development object, courses as properties with `
+var webCourse1 = 96;
+var webCourse2 = 90;
+var webCourse3 = 90;
+var webCourse4 = 82;
+var webCourse5 = 80;
+
+
+
+
 
 
 //################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
