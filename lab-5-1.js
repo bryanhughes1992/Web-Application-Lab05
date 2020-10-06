5f98c1b8ecc4ebd8a03ced1d6dd32597c4eaeee3
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-10-06 14:52:55 -0400

    change number declartion to an object

--- lab-5-1.js
+++ lab-5-1.js
@@ -2,9 +2,16 @@
 //PART 1:  AN AVERAGE FUNCTION
 //alert("Connected");//COMMENT OUT AS SOON AS YOU KNOW YOU ARE CONNECTED!!!!
 
+var numbers = {
+    num1: 302,
+    num2: 12,
+    num3: 404.66,
+    num4: 3203,
+    num5: 1203452
+};
+
 //################## CREATE YOUR AVERAGE FUNCTION
 function calcAverage(num1, num2, num3, num4, num5) {
-    var sum = num1 + num2 + num3 + num4 + num5;
     var total
 }
 
