b9562446b305c8ad82ace72c0562818b9b43c9f3
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-10-06 23:12:41 -0400

    Part 2 changes

--- lab-5-2.js
+++ lab-5-2.js
@@ -1,3 +1,16 @@
 //#### LAB 5 - FUNCTIONS & OBJECTS ####
 //PART 2:  I OBJECT!
-alert("Connected");//COMMENT OUT AS SOON AS YOU KNOW YOU ARE CONNECTED!!!!
+//alert("Connected");//COMMENT OUT AS SOON AS YOU KNOW YOU ARE CONNECTED!!!!
+var meObject = {
+    name: "Bryan Hughes",
+    dateOfBirth: "July 6th, 1992",
+    heightCm: 201,
+    weightLbs: 300,
+    greeting: `My name is ${meObject.name}, and I am ${meObject.heightCm}cm tall.`,
+    selfIntroduction: function() {
+        alert(this.greeting);
+    }
+};
+
+console.log(meObject.dateOfBirth);
+// alert(`My name is ${meObject.name}, and I am ${meObject.heightCm}cm tall.`);
